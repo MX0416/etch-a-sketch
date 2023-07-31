b08719574e4b46ee6b7193dae1e4466b02758f53
Matthew Xue <matthew.xue2004@gmail.com>
2023-07-30 18:41:08 -0700

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('#container');
+
+for (let col = 0; col < 16; col++) {
+
+  const row = document.createElement('div');
+  row.classList.add('row');
+  container.append(row);
+  for (let row = 0; row < 16; row++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    row.appendChild
+  }
+}
